cd7ee183f899b0f55c90a7c6db68ed4775b89acf
FedericoPaz <=>
2022-11-17 14:19:13 -0300

    actualizado

--- src/app/pages/home/components/products/products.component.ts
+++ src/app/pages/home/components/products/products.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html'
 })
 export class ProductsComponent {
+  sort = 'elegir';
+
+  onSortUpdated(newSort: string): void {
+    this.sort= newSort;
+
+  }
 
 }
